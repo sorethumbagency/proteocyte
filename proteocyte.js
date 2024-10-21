eb9cf6ac606fa478f8365d38ee687112718b6fa0
sorethumbagency <sarah@sorethumb.ca>
2024-10-21 13:22:07 -0400

    Update proteocyte.js
    added styles to h3

--- proteocyte.js
+++ proteocyte.js
@@ -1,12 +1,16 @@
-
   document.addEventListener('DOMContentLoaded', function() {
 
     // Change team/board/founder positions from <p> to <h3>
     const positions = Array.from(document.querySelectorAll("p.et_pb_member_position"));
   
     positions.forEach(position => {
-      position.outerHTML = '<h3>' + position.innerHTML + '</h3>';
-    });
+        const h3 = document.createElement('h3');
+        h3.innerHTML = position.innerHTML;
+        h3.style.fontWeight = '600';
+        h3.style.fontSize = '16px';
+        h3.style.color = '#1e9583';
+        position.replaceWith(h3);
+      });
 
-    
+  
   });
