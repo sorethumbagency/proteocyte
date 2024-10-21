a8e1d665e9d4045a72b40e889682020fd2cd90f0
sorethumbagency <sarah@sorethumb.ca>
2024-10-21 13:28:07 -0400

    Update proteocyte.js
    added !important to styles

--- proteocyte.js
+++ proteocyte.js
@@ -4,12 +4,10 @@
     const positions = Array.from(document.querySelectorAll("p.et_pb_member_position"));
   
     positions.forEach(position => {
-        const h3 = document.createElement('h3');
-        h3.innerHTML = position.innerHTML;
-        h3.style.fontWeight = '600';
-        h3.style.fontSize = '16px';
-        h3.style.color = '#1e9583';
-        position.replaceWith(h3);
+      const h3 = document.createElement('h3');
+      h3.innerHTML = position.innerHTML;
+      h3.setAttribute('style', 'font-weight: 600 !important; font-size: 16px !important; color: #1e9583 !important;');
+      position.replaceWith(h3);
       });
 
   
