efaa1c24858673db89d1cd2495977e9ac3652cb1
sorethumbagency <sarah@sorethumb.ca>
2024-10-21 13:41:50 -0400

    Update proteocyte.js
    back to simple version targeting DOM elements styles will be handled separately.

--- proteocyte.js
+++ proteocyte.js
@@ -1,12 +1,10 @@
- window.addEventListener('load', function() {
+ document.addEventListener('DOMContentLoaded', function() {
 
     // Change team/board/founder positions from <p> to <h3>
     const positions = Array.from(document.querySelectorAll("p.et_pb_member_position"));
-  
+
     positions.forEach(position => {
-      const h3 = document.createElement('h3');
-      h3.innerHTML = position.innerHTML;
-      h3.classList.add('custom-h3-style');  // Adding the class with important styles
-      position.replaceWith(h3);
+     position.outerHTML = '<h3>' + position.innerHTML + '</h3>';
+    });
   
   });
